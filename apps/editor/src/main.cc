ece283dd90b2265b84e9f0fa8f95573a45777ea8
raustin9 <raustin9@vols.utk.edu>
2024-08-20 12:47:40 -0400

    basic project setup

---
+++ apps/editor/src/main.cc
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include <bifrost/test.h>
+
+int main(void) {
+    std::cout << "Hello Editor\n";
+    std::cout << "Bifrost Test: " << test_function() << std::endl;
+
+    return EXIT_SUCCESS;
+}
